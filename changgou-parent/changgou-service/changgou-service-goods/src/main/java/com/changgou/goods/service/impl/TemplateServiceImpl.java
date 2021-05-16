e341dcd27bd196e965233d6aed7656654f9a6f77
tianyu-1993 <13524801404@163.com>
2021-05-16 22:05:07 +0800

    2021-05-15  docker中发布eureka

--- changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/service/impl/TemplateServiceImpl.java
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/service/impl/TemplateServiceImpl.java
@@ -6,10 +6,13 @@ import com.changgou.goods.service.TemplateService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 
+@Service
 public class TemplateServiceImpl implements TemplateService {
 
     @Autowired
@@ -23,48 +26,42 @@ public class TemplateServiceImpl implements TemplateService {
      * @return 分页结果
      */
     @Override
-    public PageInfo<Template> findPage(Template template, int page, int size) {
+    public PageInfo<Template> findPage(Template template, int page, int size){
         //分页
-        PageHelper.startPage(page, size);
+        PageHelper.startPage(page,size);
         //搜索条件构建
-        create
-        return null;
+        Example example = createExample(template);
+        //执行搜索
+        return new PageInfo<Template>(templateMapper.selectByExample(example));
     }
 
+    /**
+     * Template分页查询
+     * @param page
+     * @param size
+     * @return
+     */
     @Override
-    public PageInfo<Template> findPage(int page, int size) {
-        return null;
+    public PageInfo<Template> findPage(int page, int size){
+        //静态分页
+        PageHelper.startPage(page,size);
+        //分页查询
+        return new PageInfo<Template>(templateMapper.selectAll());
     }
 
+    /**
+     * Template条件查询
+     * @param template
+     * @return
+     */
     @Override
-    public List<Template> findList(Template template) {
-        return null;
+    public List<Template> findList(Template template){
+        //构建查询条件
+        Example example = createExample(template);
+        //根据构建的条件查询数据
+        return templateMapper.selectByExample(example);
     }
 
-    @Override
-    public void delete(Integer id) {
-
-    }
-
-    @Override
-    public void update(Template template) {
-
-    }
-
-    @Override
-    public void add(Template template) {
-
-    }
-
-    @Override
-    public Template findById(Integer id) {
-        return null;
-    }
-
-    @Override
-    public List<Template> findAll() {
-        return null;
-    }
 
     /**
      * Template构建查询对象
@@ -72,6 +69,72 @@ public class TemplateServiceImpl implements TemplateService {
      * @return
      */
     public Example createExample(Template template){
+        Example example=new Example(Template.class);
+        Example.Criteria criteria = example.createCriteria();
+        if(template!=null){
+            // ID
+            if(!StringUtils.isEmpty(template.getId())){
+                criteria.andEqualTo("id",template.getId());
+            }
+            // 模板名称
+            if(!StringUtils.isEmpty(template.getName())){
+                criteria.andLike("name","%"+template.getName()+"%");
+            }
+            // 规格数量
+            if(!StringUtils.isEmpty(template.getSpecNum())){
+                criteria.andEqualTo("specNum",template.getSpecNum());
+            }
+            // 参数数量
+            if(!StringUtils.isEmpty(template.getParaNum())){
+                criteria.andEqualTo("paraNum",template.getParaNum());
+            }
+        }
+        return example;
+    }
 
+    /**
+     * 删除
+     * @param id
+     */
+    @Override
+    public void delete(Integer id){
+        templateMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 修改Template
+     * @param template
+     */
+    @Override
+    public void update(Template template){
+        templateMapper.updateByPrimaryKey(template);
+    }
+
+    /**
+     * 增加Template
+     * @param template
+     */
+    @Override
+    public void add(Template template){
+        templateMapper.insert(template);
+    }
+
+    /**
+     * 根据ID查询Template
+     * @param id
+     * @return
+     */
+    @Override
+    public Template findById(Integer id){
+        return  templateMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * 查询Template全部数据
+     * @return
+     */
+    @Override
+    public List<Template> findAll() {
+        return templateMapper.selectAll();
     }
 }
