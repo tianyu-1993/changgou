021dd2b8d950406eecef98442aeccd660be8700c
tianyu-1993 <13524801404@163.com>
2021-07-07 00:14:17 +0800

    2021/7/6    Canal微服务搭建

--- changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/Test.java
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/Test.java
@@ -1,15 +1,28 @@
 package com.changgou.goods;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 public class Test {
 
     public static void main(String[] args){
-        String a = "002";
-        Integer b = Integer.valueOf(a);
-        int c = b + 1;
-        String d = String.valueOf(c);
-        System.out.println(d.length());
-
+        List<Integer> numList = new ArrayList<>();
+        List<Integer> backList = new ArrayList<>();
+        Random random = new Random();
+        int max = 35;
+        int min = 1;
+        for(int i=0;i<5;i++) {
+            int randomNumber = random.nextInt(max) % (max - min + 1) + min;
+            numList.add(randomNumber);
+        }
+        int a = 12;
+        int b  =1;
+        for(int j=0;j<2;j++){
+            int randomNumber = random.nextInt(a) % (a - b + 1) + b;
+            backList.add(randomNumber);
+        }
+        System.out.println(numList.toString() + backList.toString());
     }
 }
